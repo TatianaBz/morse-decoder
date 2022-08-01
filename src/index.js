e14416d4a6a4a7c16cb7bd49798bbb4b917c4667
tatiana <name>
2022-08-01 17:53:59 +0700

    decode

--- src/index.js
+++ src/index.js
@@ -38,9 +38,24 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let st = [];
+      let strExpr = expr.split('');
+      while (strExpr.length > 0) {
+        let letter = strExpr.splice(-10);
+        let world = [];
+        if (!letter.includes('*')) {
+          while (letter.length > 0 && letter.includes('1')) {
+            let simb = letter.splice(-2);
+            if (simb.join('') == '10') world.unshift('.');
+            else world.unshift('-');
+          }
+          st.unshift(MORSE_TABLE[world.join('')]);
+        }
+      else st.unshift(' ');
+      }
+      return st.join('');
 }
 
 module.exports = {
     decode
-}
+};
